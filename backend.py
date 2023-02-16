a66b6921fa0bd363a34f1adee8c614864fc2211f
joey <j.tan2231@gmail.com>
2023-02-16 12:07:44 -0500

    basic front/backend setup

---
+++ backend.py
@@ -0,0 +1,43 @@
+import sys
+import time
+import json
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+PROPAGATING = False
+CONFIG = None
+
+class ChangeHandler(FileSystemEventHandler):
+    def __init__(self):
+        super().__init__()
+
+    def on_modified(self, event):
+        global PROPAGATING
+        global CONFIG
+
+        PROPAGATING = False
+        try:
+            with open('config.json', 'r') as f:
+                CONFIG = json.load(f)
+                print(CONFIG)
+        except json.decoder.JSONDecodeError:
+            print('error reading ./config.JSON')
+
+        # reload the propagation with the updated CONFIG
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = ChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, './config.json', recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
--- index.js
+++ index.js
@@ -4,24 +4,22 @@ const qs = require('querystring');
 
 http.createServer(function (request, response) {
     if (request.method === 'GET' && request.url === '/') {
-        const path = '/home/joey/js/senior-design/index.html';
         response.writeHead(200);
+
+        const path = '/home/joey/js/senior-design/index.html';
         fs.createReadStream(path).pipe(response);
     }
     else if (request.method === 'POST' && request.url === '/save-config') {
         let body = '';
-        let post;
         request.on('data', data => body += data);
         request.on('end', () => {
             const path = '/home/joey/js/senior-design/config.json';
             fs.writeFile(path, body, (err) => {
                 if (err) {
                     console.log(err);
-                    response.writeHead(400);
                 }
                 else {
                     console.log('config updated');
-                    response.writeHead(200);
                 }
             });
         });
