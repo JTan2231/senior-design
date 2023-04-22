c6934b7570e76a0c65d5defae12e99c46fa778bc
Andy Thomas <mathman4155@gmail.com>
2023-04-21 23:32:56 -0400

    Fixed "contains" conditions

--- engine/stationary_test.py
+++ engine/stationary_test.py
@@ -25,7 +25,7 @@ longitude = float(longitude)
 
 # Setup the monitor network interface on wlan1mon
 output = subprocess.check_output(["ifconfig"])
-if output.__contains__("wlan1mon"):
+if "wlan1mon" in output:
     print("wlan1mon is already configured, skipping configuration.")
 else:
     # Check to see if any conflicting processes need to be killed before starting the WiFi Spam Script
@@ -33,7 +33,7 @@ else:
     output = subprocess.check_output(["sudo", "airmon-ng", "start", "wlan1"])
 
     output = subprocess.check_output(["ifconfig"])
-    if output.__contains__("wlan1mon"):
+    if "wlan1mon" in output:
         print("wlan1mon successfully configured.")
         output = subprocess.check_output(
             ["sudo", "service", "NetworkManager", "start"])
