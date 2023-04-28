560cbb044b5c75c19864db5afb54370b0a4c8cfc
Andy Thomas <mathman4155@gmail.com>
2023-04-27 20:07:15 -0400

    Changed network_scan to only save SSIDs for the time being

--- engine/network_scan.py
+++ engine/network_scan.py
@@ -2,7 +2,7 @@ import subprocess
 import csv
 
 
-def capture(filename):
+def propagate(filename):
     # Scan for WiFi networks
     proc = subprocess.Popen('sudo iwlist wlan0 scan',
                             stdout=subprocess.PIPE, shell=True)
@@ -13,22 +13,22 @@ def capture(filename):
 
     # Establish lists
     ssid_list = []
-    mac_list = []
-    channel_list = []
+    # channel_list = []
+    # mac_list = []
 
     # For each element in the list, find SSID and MAC address and add to appropriate list
     for line in data_list:
         if 'ESSID:' in line:
-            ssid_list.append(line.split(':')[1].strip('"'))
-        elif 'Address:' in line:
-            mac_list.append(line.split()[4])
-        elif 'Channel:' in line:
-            channel_list.append(line.split(':')[1].strip('"'))
+            curr_ssid = line.split(':')[1].strip('"').strip(",")
+            if curr_ssid not in ssid_list and '""' not in curr_ssid and "\\" not in curr_ssid:
+                ssid_list.append(curr_ssid)
+        # elif 'Address:' in line:
+        #     mac_list.append(line.split()[4])
+        # elif 'Channel:' in line:
+        #     channel_list.append(line.split(':')[1].strip('"').strip(","))
 
-  # Open CSV file and save results
-    with open('engine/lists/{}.csv'.format(filename), 'w') as outfile:
+    # Open CSV file and save results
+    with open('engine/lists/{}.txt'.format(filename), 'w') as outfile:
         writer = csv.writer(outfile)
-        for i in range(len(ssid_list)):
-            if int(channel_list[i]) > 11:
-                continue
-            writer.writerow([ssid_list[i], mac_list[i], channel_list[i]])
+        for ssid in ssid_list:
+            writer.writerow([ssid])
